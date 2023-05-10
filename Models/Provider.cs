fb56a697ad08f198e487b873c1ffcfb807c5a7c1
Kevin Agudelo <kevinagude25@gmail.com>
2023-05-09 23:10:58 -0500

    Models Pay Mode

---
+++ Models/Provider.cs
@@ -0,0 +1,16 @@
+﻿namespace SupermarketWEB.Models
+{
+    public class Provider
+    {
+        public int Id { get; set; }
+        public string Document { get; set; }
+        public string Name { get; set; }
+        public string Last_Name { get; set; }
+        public string address { get; set;}
+        public DateTime Birthday { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+
+    }
+}
