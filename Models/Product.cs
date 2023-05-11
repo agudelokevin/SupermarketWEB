eeccc51a169e8ab7b5a2a8f39fd112fed7b75b6c
Kevin Agudelo <kevinagude25@gmail.com>
2023-05-11 01:04:39 -0500

    Create Products

--- Models/Product.cs
+++ Models/Product.cs
@@ -12,6 +12,6 @@ namespace SupermarketWEB.Models
 		public decimal Price { get; set; }
 		public int Stock { get; set; }
 		public int CategoryId { get; set; }
-		public Category Category { get; set; }
+        public ICollection<Category>? Categories { get; set; }
 	}
 }
