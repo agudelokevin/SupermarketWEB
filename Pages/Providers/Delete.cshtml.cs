9d773e3cef7eaa5e02feb861cf3d0a20dc2c1655
Kevin Agudelo <kevinagude25@gmail.com>
2023-05-12 00:04:02 -0500

    Delete Providers

---
+++ Pages/Providers/Delete.cshtml.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Providers
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly SumpermarketContext _context;
+        public DeleteModel(SumpermarketContext context)
+        {
+            _context = context;
+        }
+        [BindProperty]
+        public Provider Provider { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Providers == null)
+            {
+                return NotFound();
+            }
+            var provider = await _context.Providers.FindAsync(id);
+
+            if (provider != null)
+            {
+                Provider = provider;
+                _context.Providers.Remove(provider);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
